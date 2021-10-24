db59f6bb43bd9d167cdd6a551c96db6ccd2e8d75
Dylan <dcren2010@gmail.com>
2021-10-24 17:19:41 -0400

    create various components for menu items

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import { Switch, BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
-import { Navbar } from './components';
-import './App.css'
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
+import './App.css';
 
 const App = () => {
 	return (
 		<Router>
-			
-		<div className='app'>
-			<div className="navbar">
-				<Navbar />
-			</div>
-			<div className="main">
+			<div className='app'>
+				<div className='navbar'>
+					<Navbar />
+				</div>
 
+				<div className='main'>
+					<Layout>
+						<div className='routes'>
+							<Switch>
+								<Route exact path='/'>
+									<Homepage />
+								</Route>
+
+								<Route exact path='/exchanges'>
+									<Exchanges />
+								</Route>
+
+								<Route exact path='/cryptocurrencies'>
+									<Cryptocurrencies />
+								</Route>
+
+								{/* coin detail is dynamic based on id */}
+								<Route exact path='/crypto/:coinId'>
+									<CryptoDetails />
+								</Route>
+
+								<Route exact path='/news'>
+									<News />
+								</Route>
+							</Switch>
+						</div>
+					</Layout>
+				</div>
+
+				<div className='footer'></div>
 			</div>
-			<div className="footer">
-				
-			</div>
-		</div>
 		</Router>
-	)
-}
+	);
+};
 
-export default App
+export default App;
