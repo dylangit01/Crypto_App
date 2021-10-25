7ba1c5c74c21c6fd727d38cc9c77851444a12d20
Dylan <dcren2010@gmail.com>
2021-10-25 07:13:11 -0400

    add top 10 crypto and news sections in homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
+import {Cryptocurrencies, News } from './index'
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
@@ -35,6 +36,17 @@ const Homepage = () => {
 					<Statistic title='Total Markets' value={millify(globalStats.totalMarkets)} />
 				</Col>
 			</Row>
+			<div className="home-heading-container">
+				<Title level={2} className='home-title'> Top 10 Cryptocurrencies in the world </Title>
+				<Title level={3} className='show-more'> <Link to='/cryptocurrencies'>Show more</Link> </Title>
+			</div>
+			<Cryptocurrencies />
+
+			<div className="home-heading-container">
+				<Title level={2} className='home-title'> Latest Crypto News </Title>
+				<Title level={3} className='show-more'> <Link to='/news'>Show more</Link> </Title>
+			</div>
+			<News />
 		</>
 	);
 }
