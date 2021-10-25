384f31c593b13a893d17cc5040d17175e94d2156
Dylan <dcren2010@gmail.com>
2021-10-25 07:04:32 -0400

    using redux hook to fetch crypto stats in Homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -9,22 +9,34 @@ const { Title } = Typography;
 
 const Homepage = () => {
 	// Use redux hook syntax is simple:
-	const { data, isFetching } = useGetCryptosQuery();
-
-	console.log(data);
+	const { data, isFetching } = useGetCryptosQuery(); 
+	const globalStats = data?.data?.stats;
+	if(isFetching) return 'Loading...'
 
 	return (
 		<>
-			<Title level={2} className='heading'>Global Crypto Stats</Title>
+			<Title level={2} className='heading'>
+				Global Crypto Stats
+			</Title>
 			<Row>
-				<Col span={12}><Statistic title='Total CryptoCurrencies' value='5'/></Col>
-				<Col span={12}><Statistic title='Total Exchanges' value='5'/></Col>
-				<Col span={12}><Statistic title='Total Market Cap' value='5'/></Col>
-				<Col span={12}><Statistic title='Total 24h Volume' value='5'/></Col>
-				<Col span={12}><Statistic title='Total Markets' value='5'/></Col>
+				<Col span={12}>
+					<Statistic title='Total CryptoCurrencies' value={globalStats.total} />
+				</Col>
+				<Col span={12}>
+					<Statistic title='Total Exchanges' value={millify(globalStats.totalExchanges)} />
+				</Col>
+				<Col span={12}>
+					<Statistic title='Total Market Cap' value={millify(globalStats.totalMarketCap)} />
+				</Col>
+				<Col span={12}>
+					<Statistic title='Total 24h Volume' value={millify(globalStats.total24hVolume)} />
+				</Col>
+				<Col span={12}>
+					<Statistic title='Total Markets' value={millify(globalStats.totalMarkets)} />
+				</Col>
 			</Row>
 		</>
-	)
+	);
 }
 
 export default Homepage
